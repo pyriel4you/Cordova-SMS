3248a741175d98065ed2be0b93906cd850349b65
Adrian <pyriel4you@users.noreply.github.com>
2017-07-22 17:32:31 +0300

    Update smsplugin.js

--- www/smsplugin.js
+++ www/smsplugin.js
@@ -1,30 +1,31 @@
-var argscheck = require('cordova/argscheck'),
-    exec = require('cordova/exec');
+'use strict';
 
-var smsplugin = {
-	send:function (phoneNumber, message, method, successCallback, failureCallback) {    
-		return exec(successCallback, failureCallback, 'SmsPlugin', 'SEND_SMS', [phoneNumber, message, method]);
-	},
+var exec = require('cordova/exec');
+
+var sms = {};
+
+sms.send = function (phoneNumber, message, method, successCallback, failureCallback) {    
+	return exec(successCallback, failureCallback, 'SmsPlugin', 'SEND_SMS', [phoneNumber, message, method]);
+};
 	//Check if the device has a possibility to send and receive SMS
-	isSupported:function(successCallback,failureCallback) {
-		return exec(successCallback, failureCallback, 'SmsPlugin', 'HAS_SMS_POSSIBILITY', []);
-	},
+sms.isSupported = function(successCallback,failureCallback) {
+	return exec(successCallback, failureCallback, 'SmsPlugin', 'HAS_SMS_POSSIBILITY', []);
+};
 	//Get SIM phone number
-	getNumber:function(successCallback,failureCallback) {
-		return exec(successCallback, failureCallback, 'SmsPlugin', 'GET_NUMBER', []);
-	},
+sms.getNumber = function(successCallback,failureCallback) {
+	return exec(successCallback, failureCallback, 'SmsPlugin', 'GET_NUMBER', []);
+};
 	//Get MCC+MNC
-	getMnc:function(successCallback,failureCallback) {
-		return exec(successCallback, failureCallback, 'SmsPlugin', 'GET_MNC', []);
-	},
+sms.getMnc = function(successCallback,failureCallback) {
+	return exec(successCallback, failureCallback, 'SmsPlugin', 'GET_MNC', []);
+};
 	//Start receiving sms, and the successCallback function receives one string as parameter formatted such as [phonenumber]>[message]
-	startReception:function(successCallback,failureCallback) {
-		return exec(successCallback, failureCallback, 'SmsPlugin', 'RECEIVE_SMS', []);
-	},
+sms.startReception = function(successCallback,failureCallback) {
+	return exec(successCallback, failureCallback, 'SmsPlugin', 'RECEIVE_SMS', []);
+};
 	//Stop receiving sms
-	stopReception:function(successCallback,failureCallback) {
-		return exec(successCallback, failureCallback, 'SmsPlugin', 'STOP_RECEIVE_SMS', []);
-	}
+sms.stopReception:function(successCallback,failureCallback) {
+	return exec(successCallback, failureCallback, 'SmsPlugin', 'STOP_RECEIVE_SMS', []);
 };
 
-module.exports=smsplugin;
+module.exports=sms;
