6fc7af1222ac4d3dc226d3006b15d144cc65ffb2
Adrian <pyriel4you@users.noreply.github.com>
2017-07-22 17:33:19 +0300

    Update smsplugin.js

--- www/smsplugin.js
+++ www/smsplugin.js
@@ -2,30 +2,30 @@
 
 var exec = require('cordova/exec');
 
-var sms = {};
+var smsplugin = {};
 
-sms.send = function (phoneNumber, message, method, successCallback, failureCallback) {    
+smsplugin.send = function (phoneNumber, message, method, successCallback, failureCallback) {    
 	return exec(successCallback, failureCallback, 'SmsPlugin', 'SEND_SMS', [phoneNumber, message, method]);
 };
 	//Check if the device has a possibility to send and receive SMS
-sms.isSupported = function(successCallback,failureCallback) {
+smsplugin.isSupported = function(successCallback,failureCallback) {
 	return exec(successCallback, failureCallback, 'SmsPlugin', 'HAS_SMS_POSSIBILITY', []);
 };
 	//Get SIM phone number
-sms.getNumber = function(successCallback,failureCallback) {
+smsplugin.getNumber = function(successCallback,failureCallback) {
 	return exec(successCallback, failureCallback, 'SmsPlugin', 'GET_NUMBER', []);
 };
 	//Get MCC+MNC
-sms.getMnc = function(successCallback,failureCallback) {
+smsplugin.getMnc = function(successCallback,failureCallback) {
 	return exec(successCallback, failureCallback, 'SmsPlugin', 'GET_MNC', []);
 };
 	//Start receiving sms, and the successCallback function receives one string as parameter formatted such as [phonenumber]>[message]
-sms.startReception = function(successCallback,failureCallback) {
+smsplugin.startReception = function(successCallback,failureCallback) {
 	return exec(successCallback, failureCallback, 'SmsPlugin', 'RECEIVE_SMS', []);
 };
 	//Stop receiving sms
-sms.stopReception:function(successCallback,failureCallback) {
+smsplugin.stopReception:function(successCallback,failureCallback) {
 	return exec(successCallback, failureCallback, 'SmsPlugin', 'STOP_RECEIVE_SMS', []);
 };
 
-module.exports=sms;
+module.exports=smsplugin;
